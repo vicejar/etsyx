febc47cc0126935067ff19ca97bac5d8f784b4d7
Your Actual Name <Your Actual Email>
2014-07-25 11:26:18 -0700

    add figaro gem

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,3 +1,5 @@
 class Listing < ActiveRecord::Base
-	has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "https://dl.dropboxusercontent.com/s/sm3ja2rreztsaj4/blue_dropbox_glyph-vflJ8-C5d.jpg"
+  has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "default.jpg",
+      :storage => :dropbox,
+      :dropbox_credentials => Rails.root.join("config/dropbox.yml")
 end
