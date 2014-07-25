7f90b9beddfc07cbd529ae148f576fbf23057dc2
Your Actual Name <Your Actual Email>
2014-07-25 16:16:31 -0700

    devise installed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Etsyx::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+    # Required for Devise gem > remember to change local host 3000 to actual application host
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Etsyx::Application.routes.draw do
+  devise_for :users
   resources :listings
 
   get "pages/about"
